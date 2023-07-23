7839b9ff1d02e28739be90edc760977e4b8f612b
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-07-23 15:00:05 +0600

    Navber part is added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navber from '../Components/Navber/Navber';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            
+            <Navber></Navber>
+            {/* <div className='container mx-auto'> */}
+                <Outlet></Outlet>
+            {/* </div> */}
         </div>
     );
 };
