cfd549b47ed862d84a9e3d50fce4a45c9c45c51e
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-07-23 14:13:46 +0600

    JobInfo project created

---
+++ src/Layout/Container.jsx
@@ -0,0 +1,12 @@
+import { Children } from "react";
+
+
+const Container = () => {
+    return (
+        <div className='max-w-7xl'>
+            {Children}
+        </div>
+    );
+};
+
+export default Container;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main/>,
+        // errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+]);
